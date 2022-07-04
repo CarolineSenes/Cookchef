a5783bc124e6b6684f10c1bc7714a97560534e42
unknown <quentindevial19@gmail.com>
2022-07-04 11:49:43 +0200

    projet partie 3 cookchef

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Homepage from "./pages/Homepage/Homepage";
+import styles from "./App.module.scss";
+// import { seedRecipes } from "./data/seed";
+
+// seedRecipes();
+
+function App() {
+  return (
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      <Homepage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
