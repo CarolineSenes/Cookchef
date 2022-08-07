45ff719cbaade18c8c36502d98fff7ccaf2468c3
Caroline SENES <caro67200@gmail.com>
2022-08-07 12:06:45 +0200

    init base layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import styles from './App.module.scss';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>App</h1>
+      <div className={`d-flex flex-column ${styles.appContainer}`}>
+        <Header />
+        <Content />
+        <Footer />
       </div>
     );
   }
