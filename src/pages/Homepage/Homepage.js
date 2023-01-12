b410c0767f2bd8231cf928bfe56357f291f5feeb
Caroline SENES <caroline.senes@capgemini.com>
2023-01-12 09:41:21 +0100

    refacto home page searchbar

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,9 +1,12 @@
+import { useState, useEffect, useContext } from "react";
+
 import styles from "./Homepage.module.scss";
+
 import Recipe from "./components/Recipe/Recipe";
-import { useState } from "react";
+import Search from "./components/Search/Search";
+
 import Loading from "../../components/Loading/Loading";
-import { useEffect } from "react";
-import { useContext } from "react";
+
 import { ApiContext } from "../../context/ApiContext";
 
 function Homepage() {
@@ -47,11 +50,6 @@ function Homepage() {
     );
   }
 
-  function handleInput(e) {
-    const filter = e.target.value;
-    setFilter(filter.trim().toLowerCase());
-  }
-
   return (
     <div className="flex-fill container d-flex flex-column p-20">
       <h1 className="my-30">
@@ -61,17 +59,7 @@ function Homepage() {
       <div
         className={`card flex-fill d-flex flex-column p-20 mb-20 ${styles.contentCard}`}
       >
-        <div
-          className={`d-flex flex-row justify-content-center align-item-center my-30 ${styles.searchBar}`}
-        >
-          <i className="fa-solid fa-magnifying-glass mr-15"></i>
-          <input
-            onInput={handleInput}
-            className="flex-fill"
-            type="text"
-            placeholder="Rechercher"
-          />
-        </div>
+        <Search setFilter={setFilter} />
         {isLoading && !recipes.length ? (
           <Loading />
         ) : (
