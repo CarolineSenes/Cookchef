8a58908e10dfb7c017990e55ee29d1ea436dd54e
Caroline SENES <caroline.senes@capgemini.com>
2023-02-13 10:23:47 +0100

    display recipes list in admin page

--- src/pages/Admin/pages/AdminRecipes/pages/AdminRecipesList/AdminRecipesList.js
+++ src/pages/Admin/pages/AdminRecipes/pages/AdminRecipesList/AdminRecipesList.js
@@ -1,5 +1,22 @@
+import { useFetchRecipes } from "../../../../../../hooks";
+import styles from "./AdminRecipesList.module.scss";
+
 function AdminRecipesList() {
-  return <h1>Admin Recipes List</h1>;
+  const [[recipes]] = useFetchRecipes();
+
+  return (
+    <ul className={styles.list}>
+      {recipes.length
+        ? recipes.map((r, i) => (
+            <li key={`${i}-${r._id}`} className="d-flex align-items-center">
+              <span className="flex-fill">{r.title}</span>
+              <button className="btn btn-primary mr-15">Editer</button>
+              <button className="btn btn-danger">Supprimer</button>
+            </li>
+          ))
+        : null}
+    </ul>
+  );
 }
 
 export default AdminRecipesList;
